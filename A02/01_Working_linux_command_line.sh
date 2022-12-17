a88773dc41e6a6f85eacbc6450e9080a615b5e84
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-17 15:15:15 +0100

    new files

---
+++ A02/01_Working_linux_command_line.sh
@@ -0,0 +1,74 @@
+#Check the terminal session
+tty
+
+#Get details about it
+ls -l /dev/pts0
+
+#In another way
+ls -l $(tty)
+
+#Lets have a look at the metadata
+
+#Install tree
+
+sudo dnf install -y tree
+
+#With tree we will have a look at the FHS (Filesystem Hierarchy Standard)
+
+tree -L 1 /
+
+#So then in /dev we will find devices
+ls -l /dev
+
+#With the tree command
+tree /dev
+
+#Infos about the directory
+ls -ld /dev
+
+#If the directory has execute perms we can cd into it
+cd /dev
+
+#The working directory
+pwd
+
+#With cd without argument gets to home diretory
+cd
+
+#cd - gets back
+cd -
+
+#Why, its in a variable
+echo $OLDPWD 
+
+cd $OLDPWD or cd -
+
+#Lets create a directory
+mkdir test
+
+#About the directory
+ls -ld test
+
+#There are 2 hardlinks
+ls -ld test/.
+
+#Create a path
+mkdir -p test/d1/d2
+
+#We proof
+tree test
+
+#Another way
+mkdir test/d{10..19}
+
+#We proof
+tree test
+
+#To remove
+rm -rf test
+
+
+
+Some of the very basic Linux operations involve the cd command. I am sure this is basic to
+you but lets look at some things you may not know.
+
---
+++ A02/02_Working_file_metadata.sh
@@ -0,0 +1,80 @@
+#Start with ls and the last edited files
+ls -ltr /etc
+
+#The last edited files, really?
+sudo vi /etc/hosts
+
+#Add new line with 8.8.8.8 google
+
+#ls again
+ls -ltr /etc
+
+#A new file with touch
+touch file1 
+
+#Metadata
+ls -l file1
+
+#With the file command
+file file1
+
+#The statistics with stat
+stat file1 
+
+#What will change if we cat the file
+cat file1
+
+#The statistics with stat
+stat file1
+
+#Some text
+echo hello >> file1
+
+#What about the stats
+stat file1
+
+#We change the mode
+chmod 770 file1
+
+#What about the stats
+stat file1
+
+#Show some values
+stat -c %x file1 #Access 
+stat -c %y file1 #Modify 
+stat -c %z file1 #Change
+
+#If we copy
+cp file1 file2
+
+#Stat file2
+stat file2
+
+#To keep the metadata
+cp -a file1 file3
+
+#The stats
+stat file3
+
+#Move a file (rename in this case)
+mv file1 file4
+
+#List the files
+ls file*
+
+#Stat file4
+stat file4
+
+#Copy from another directory
+cp -a /etc/hosts .
+
+#Stat hosts
+stat hosts
+
+
+
+Field              Meaning
+
+Access             Last read
+Modify             Data last modified
+Change             Metadata last changed
