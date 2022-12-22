cfbd6e255e30140090e6d81653a5e21026e338a4
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-22 13:07:47 +0100

    new files

---
+++ A05/02_Working_xfs_filesystem.sh
@@ -0,0 +1,29 @@
+#It is easier in a root session
+sudo -i
+
+#Diskspace and filesystem
+df -hT /
+
+#We use xfs_info
+xfs_info /
+
+#Online defragmentation
+xfs_fsr -v /
+
+#Start again no defrag is needed
+xfs_fsr -v /
+
+#Create a xfs filesystem on loop1p2
+mkfs -t xfs /dev/loop1p2
+
+#Set the label
+xfs_admin -L mkt /dev/loop1p2
+
+#Get the infos
+xfs_info /dev/loop1p2
+
+
+
+The xfs filesystem is the default in RHEL 8 and Alma Linux. The filesystem can be defragmented
+online using xfs_fsr. Again, the volume label can be set during the creation of the filesystem or
+later, this time using xfs_admin.
---
+++ A05/03_Working_btrfs_filesystem.sh
@@ -0,0 +1,47 @@
+#We are working on openSUSE
+
+#It is easier in a root session
+sudo -i
+
+#To get the btrfs support we install the package
+zypper install -y btrfsprogs
+
+#Create two new disk file
+fallocate -l1G disk1
+fallocate -l1G disk2
+
+#Link the device to the file
+losetup -f --show disk1 
+losetup -f --show disk2 
+
+#Create the filesystem
+mkfs -t btrfs /dev/loop0
+
+#Create a directory
+mkdir /data
+
+#Mount the device
+mount /dev/loop0 data
+
+#Check with df
+df -h
+df -h /data
+
+#Get infos
+btrfs filesystem show /data
+
+#Add a device
+btrfs device add /dev/loop1 /data
+
+#Get infos
+btrfs filesystem show /data
+
+#What about the diskspace
+df -h /data
+
+
+
+The btrfs filesystem (B-Tree Filesystem) is newer and based around the ReiserFS which was big
+in SUSE and ZFS which was developed in Solaris. It is no longer included in RHEL 8 but can be
+installed in openSUSE or Ubuntu; we use it in openSUSE. One big advantage is that we can add
+additional storage to existing live filesystems.
