6290c8313cc8fb1b49a1f4066d4cff8c8fbd31ab
Tom Wechsler <tom.wechsler@outlook.com>
2022-12-26 13:44:11 +0100

    new file

---
+++ A10/01_Software_ubuntu.sh
@@ -0,0 +1,44 @@
+#We are working on ubuntu in a root session
+sudo -i
+
+#Lets look at the source
+ls /etc/apt/sources*
+
+#Cat the config file
+cat /etc/apt/sources.list
+
+#Some much text
+grep -vE '^(#|$)' cat /etc/apt/sources.list
+
+#The last metadata update
+stat -c %z /var/lib/apt/periodic/update-success-stamp
+
+#Update the cache
+apt update
+
+#The last metadata update
+stat -c %z /var/lib/apt/periodic/update-success-stamp
+
+#Search for package
+apt search tree
+
+#Much better
+apt search tree '^tree$'
+
+#Install tree
+apt install -y tree
+
+#Does tree work?
+tree -L 1 /
+
+#Remove with apt (does not remove config files)
+apt remove tree
+
+#Remove with apt (does also remove config files)
+apt purge tree
+
+#What is upgradable
+apt list --upgradable
+
+#Give a go
+apt upgrade -y
